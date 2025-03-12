f0f24c9f8456bdda47fe46db1f76428d2da37435
Ania <aniaa.pawlik@gmail.com>
2025-03-12 13:30:16 +0100

    Revert "Add input password"
    This reverts commit 383e6b66283c2ef8cfbbced69dc075fe4b639456.

--- my-app/src/containers/JobBoard.tsx
+++ my-app/src/containers/JobBoard.tsx
@@ -117,7 +117,6 @@ const JobBoard: FC = () => {
       <Title>Job Board</Title>
       <StyledWrapperLogin>
         <input type="text" placeholder="Login" />
-        <input type="password" placeholder="Password" />
       </StyledWrapperLogin>
       <StyledWrapperSelect>
         {Object.entries(selectData).map(([key, jobData]) => (
